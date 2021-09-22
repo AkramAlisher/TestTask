b5fe5b4a7e9fb6de92128517f486dcf5fdc954e6
Akram Alisher <akramch228@gmail.com>
2021-09-22 08:17:25 +0600

    init

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import sqlite3 from 'sqlite3';
+
+const PORT = 5000;
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, () => console.log('zbs'));
+
+let db = new sqlite3.Database('./db.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
+app.post('appointments/', (req, res) => {
+    console.log(req.body);
+    res.status(200);
+})
